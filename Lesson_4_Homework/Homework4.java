16075baa979c0253f6a6d7a003b611b6efd9117e
AleksOstr <ostryankoam@yandex.ru>
2022-12-05 13:25:12 +0400

    Add sort by age

--- Lesson_4_Homework/Homework4.java
+++ Lesson_4_Homework/Homework4.java
@@ -6,7 +6,6 @@
 
 package JavaEd.Lesson_4_Homework;
 
-import java.sql.Array;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -58,5 +57,35 @@ public class Homework4 {
             }
         }
 
+        for (int i = 0; i < listAge.size() - 1; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < listAge.size(); j++) {
+                if (listAge.get(j) < listAge.get(minIndex)) minIndex = j;
+            }
+            int temp = listAge.get(i);
+            listAge.set(i, listAge.get(minIndex));
+            listAge.set(minIndex, temp);
+
+            String temp2 = listFirstName.get(i);
+            listFirstName.set(i, listFirstName.get(minIndex));
+            listFirstName.set(minIndex, temp2);
+
+            temp2 = listSecondName.get(i);
+            listSecondName.set(i, listSecondName.get(minIndex));
+            listSecondName.set(minIndex, temp2);
+
+            temp2 = listLastName.get(i);
+            listLastName.set(i, listLastName.get(minIndex));
+            listLastName.set(minIndex, temp2);
+
+            boolean temp3 = listGender.get(i);
+            listGender.set(i, listGender.get(minIndex));
+            listGender.set(minIndex, temp3);
+        }
+        for (int i = 0; i < listAge.size(); i++) {
+            System.out.printf("%s %s %s %d %s%n",
+                    listFirstName.get(i), listSecondName.get(i), listLastName.get(i), listAge.get(i),
+                    (listGender.get(i) ? "Male":"Female"));
+        }
     }
 }
