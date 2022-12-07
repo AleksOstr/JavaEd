0a19dac7b9d5119e82046f580b80744a464b0746
AleksOstr <ostryankoam@yandex.ru>
2022-12-07 13:44:13 +0400

    Done

--- Lesson_5_Homework/Homework5.java
+++ Lesson_5_Homework/Homework5.java
@@ -35,5 +35,49 @@ public class Homework5 {
 
         tMap.forEach((k, v) -> tMap.put(k, v + "!"));
         System.out.println(tMap);
+
+        while (hMap.size() < 1000) {
+            int randomInt = new Random().nextInt(2000);
+            hMap.putIfAbsent(randomInt, "Simple string");
+        }
+
+        while (tMap.size() < 1000) {
+            int randomInt = new Random().nextInt(2000);
+            tMap.putIfAbsent(randomInt, "String");
+        }
+
+        long start = System.currentTimeMillis();
+        hMap.forEach((k, v) -> System.out.printf("Key: %d -> Value: %s ", k, v));
+        System.out.println();
+        System.out.println("HashMap forEach print time: " + (System.currentTimeMillis() - start));
+
+        start = System.currentTimeMillis();
+        int count = 0;
+        while (count < 1000) {
+            int randomInt = new Random().nextInt(2000);
+            if (hMap.containsKey(randomInt)) {
+                System.out.printf("Key: %d -> Value: %s ", randomInt, hMap.get(randomInt));
+                count++;
+            }
+        }
+        System.out.println();
+        System.out.println("HashMap random print time: " + (System.currentTimeMillis() - start));
+
+        start = System.currentTimeMillis();
+        tMap.forEach((k, v) -> System.out.printf("Key: %d -> Value: %s ", k, v));
+        System.out.println();
+        System.out.println("TreeMap forEach print time: " + (System.currentTimeMillis() - start));
+
+        start = System.currentTimeMillis();
+        count = 0;
+        while (count < 1000) {
+            int randomInt = new Random().nextInt(2000);
+            if (tMap.containsKey(randomInt)) {
+                System.out.printf("Key: %d -> Value: %s ", randomInt, tMap.get(randomInt));
+                count++;
+            }
+        }
+        System.out.println();
+        System.out.println("TreeMap random print time: " + (System.currentTimeMillis() - start));
     }
 }
